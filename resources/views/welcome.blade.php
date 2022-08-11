99563626b7b931c396e8fface5e98eed05a3aa30
BikmanDjuma <bikmangeek@gmail.com>
2022-08-11 04:34:05 -0700

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
                 @foreach($item as $product)
                     <div class="col-md-3 col-sm-6 col-xs-12">
                         <div class="content-right-product">
-                            <div><img src="{{URL::to('/')}}/images/product/{{$product->image}}" class="img-responsive" alt="Top Shoes"></div>
+                            <div><img src="{{asset('/images/product/'.$product->image)}}" class="img-responsive" alt="Top Shoes"></div>
                             <div class="row">
                                 <div class="col-md-12 text-center">
                                     <div class="content-right-product-h4 text-center">{{$product->name}}</div>
