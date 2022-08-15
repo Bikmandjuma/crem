61055b464b49c07ae9d19b0344d862353357f6dd
BikmanDjuma <bikmangeek@gmail.com>
2022-08-15 02:19:41 -0700

    new staff

--- resources/views/Pages/cover.blade.php
+++ resources/views/Pages/cover.blade.php
@@ -158,7 +158,7 @@ use App\Models\Order;
                                 </div>
                             @else
                                 <i class="fa fa-cart-plus cart_size"></i> &nbsp; 
-                                <span class="badge badge-danger" style="margin-top:-30px;margin-left:-10px;">0</span><p>Shopping cart/Orders</p>
+                                <span class="badge badge-danger" style="margin-top:-30px;margin-left:-10px;">0</span><p>Shopping cart</p>
                             @endauth
                         </div>
                     </div>
