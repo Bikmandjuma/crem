abfd9e3933f7cc2b14aba887b2467fd11eb338a6
BikmanDjuma <bikmangeek@gmail.com>
2022-08-19 02:00:57 -0700

    at 19/8

--- resources/views/customer/order.blade.php
+++ resources/views/customer/order.blade.php
@@ -50,8 +50,8 @@ use Illuminate\Support\Facades\DB;
 
         $customer_id=auth()->guard('customer')->user()->id;
         
-        $total_amount_checkout=DB::table('orders')->where('customer_id',$customer_id)->where('payment_checkout','')->sum('orders.amount');
-        $total_product_to_buy=DB::table('orders')->where('customer_id',$customer_id)->where('payment_checkout','')->sum('orders.product_counts');
+        $total_amount_checkout=DB::table('orders')->where('customer_id',$customer_id)->where('payment_checkout','')->sum('amount');
+        $total_product_to_buy=DB::table('orders')->where('customer_id',$customer_id)->where('payment_checkout','')->sum('product_counts');
 
     ?>
 
